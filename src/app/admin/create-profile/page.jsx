e2d4bd6c4b24de99ee0f687da63f564ad2e67a14
conuvedevegana <cardelsil@gmail.com>
2024-04-04 12:54:11 +0200

    created sector button component

--- src/app/admin/create-profile/page.jsx
+++ src/app/admin/create-profile/page.jsx
@@ -1,7 +1,14 @@
+import InfoButton from '@/app/components/Buttons/InfoButton'
+import { Container, Typography } from '@mui/material'
 import React from 'react'
 
 export default function CreateProfile() {
   return (
-    <div>page</div>
+    <Container>
+        <Typography variant='h1' justifyContent='center'>
+            Crear perfil de empresa
+        </Typography>
+        <InfoButton/>
+    </Container>
   )
 }
