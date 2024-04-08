f95facae69f2d2401aae1cafb0f0e605783e66b1
melitacasola <melissacasola@gmail.com>
2024-04-08 13:16:22 +0200

    fix: added logic img en proposalCard

--- src/app/components/ProposalCards/ProposalCard.jsx
+++ src/app/components/ProposalCards/ProposalCard.jsx
@@ -34,7 +34,7 @@ export default function ProposalCard({ card }) {
       <Card elevation={2} className="max-w-xs h-auto">
         <CardMedia>
           <Image
-            src={ card.image && `${URL_IMG}/${card.image}`}
+            src={card.image && (card.image.startsWith('http') ? card.image : `${URL_IMG}/${card.image}`)}
             width={360}
             height={400}
             alt={`Imagen acerca de ${card.brand}`}
