fa0df2bc105d10592303c28591eb48d80f7bfd75
lolamindi <laura_mday@yahoo.co.uk>
2024-04-08 20:48:06 +0200

    adjust spinner size

--- src/app/components/Spinner.jsx
+++ src/app/components/Spinner.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { PuffLoader } from "react-spinners";
+import { BeatLoader } from "react-spinners";
 
 function Spinner() {
   return (
-    <div className="spinner mt-60 mb-60">
-      <PuffLoader color="#46A9B6" margin={2} size={16} speedMultiplier={1} />
+    <div className="spinner mt-20 mb-60">
+      <BeatLoader color="#06ABDC" margin={2} size={20} speedMultiplier={1} />
     </div>
   );
 }
