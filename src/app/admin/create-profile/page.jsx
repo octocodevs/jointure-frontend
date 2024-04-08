c1419ebe1f2736c7c964ecf7caa52f88bb423938
conuvedevegana <cardelsil@gmail.com>
2024-04-08 15:45:48 +0200

    added image upload component

--- src/app/admin/create-profile/page.jsx
+++ src/app/admin/create-profile/page.jsx
@@ -1,6 +1,7 @@
 import { Container, Typography, Button } from '@mui/material'
 import React from 'react'
 import CreateProfileForm from '@/app/components/InfoForms/CreateProfileForm'
+import ImageUpload from '@/app/components/Buttons/ImageUpload'
 
 export default function CreateProfile() {
   return (
@@ -9,6 +10,8 @@ export default function CreateProfile() {
         <Typography variant='h1' color="primary" justifyContent='center'>
             Crear perfil de empresa
         </Typography>
+
+  
         <CreateProfileForm />
 
         
--- src/app/components/InfoForms/CreateProfileForm.jsx
+++ src/app/components/InfoForms/CreateProfileForm.jsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { Container, TextField, Button, MenuItem } from '@mui/material';
-import React, { useState } from 'react'
-import OptionList from './OptionList';
+import { Container, TextField, Button, MenuItem, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import ImageUpload from '../Buttons/ImageUpload';
 
 
 
@@ -232,30 +232,30 @@ export default function CreateProfileForm() {
   const handleSubmit = async () => {
 
 
-      const profileData = {
-          image: image,  
-          CIF: cifValue,
-          legal_structure: legalStructure,
-          phone_number: phone,
-          email_contact: contactMail,
-          sector: sector,
-          activity: activity,
-          values: values,
-          business_size: businessSize,
-          market: market,
-          clients: clients,
-          sales_channel: salesChannels,
-          description: description,
-          social_network_instagram: instagram,
-          social_network_linkedin: linkedin,
-          social_network_x: socialX,
-          social_network_facebook: facebook,
-          social_network_tiktok: tiktok,
-          social_network_spotify: spotify,
-          social_network_youtube: youtube,
-          social_network_pinterest: pinterest,
+    const profileData = {
+      image: image,
+      CIF: cifValue,
+      legal_structure: legalStructure,
+      phone_number: phone,
+      email_contact: contactMail,
+      sector: sector,
+      activity: activity,
+      values: values,
+      business_size: businessSize,
+      market: market,
+      clients: clients,
+      sales_channel: salesChannels,
+      description: description,
+      social_network_instagram: instagram,
+      social_network_linkedin: linkedin,
+      social_network_x: socialX,
+      social_network_facebook: facebook,
+      social_network_tiktok: tiktok,
+      social_network_spotify: spotify,
+      social_network_youtube: youtube,
+      social_network_pinterest: pinterest,
 
-      };
+    };
 
     axios.get('/sanctum/csrf-cookie').then(response => {
       registerNewProfile(profileData).then((res) => {
@@ -278,35 +278,42 @@ export default function CreateProfileForm() {
         onSubmit={handleSubmit}
       >
 
+        <ImageUpload />
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="cif"
-          label="CIF"
-          variant="outlined"
-          color="secondary"
-          required
-          helperText="Escribe tu CIF"
-          onChange={(e) => setCifValue(e.target.value)}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="cif"
+              label="CIF"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+              helperText="Escribe tu CIF"
+              onChange={(e) => setCifValue(e.target.value)}
+            />
+          </Grid>
 
-        />
-
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          className="m-4"
-          id="phone_number"
-          label="Número de teléfono"
-          variant="outlined"
-          color="primary"
-          required
-          helperText="Escribe tu número de teléfono"
-          onChange={(e) => setPhone(e.target.value)}
-/*                         error={phoneError}
- */                    />
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              className="m-4"
+              id="phone_number"
+              label="Número de teléfono"
+              variant="outlined"
+              color="primary"
+              fullWidth
+              required
+              helperText="Escribe tu número de teléfono"
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </Grid>
+        </Grid>
 
         <TextField
           sx={{
@@ -322,57 +329,74 @@ export default function CreateProfileForm() {
 
         />
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="social_network_instagram"
-          label="Instagram"
-          variant="outlined"
-          color="secondary"
-          helperText="Escribe el link a tu Instagram"
-          onChange={(e) => setInstagram(e.target.value)}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="social_network_instagram"
+              label="Instagram"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              helperText="Escribe el link a tu Instagram"
+              onChange={(e) => setInstagram(e.target.value)}
 
-        />
+            />
+          </Grid>
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="social_network_tiktok"
-          label="TikTok"
-          variant="outlined"
-          color="secondary"
-          helperText="Escribe el link a tu TikTok"
-          onChange={(e) => setTiktok(e.target.value)}
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="social_network_tiktok"
+              label="TikTok"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              helperText="Escribe el link a tu TikTok"
+              onChange={(e) => setTiktok(e.target.value)}
 
-        />
-   <br></br>
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="social_network_x"
-          label="X"
-          variant="outlined"
-          color="secondary"
-          helperText="Escribe el link a tu X"
-          onChange={(e) => setSocialX(e.target.value)}
+            />
+          </Grid>
+        </Grid>
 
-        />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="social_network_x"
+              label="X"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              helperText="Escribe el link a tu X"
+              onChange={(e) => setSocialX(e.target.value)}
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="social_network_linkedin"
-          label="LinkedIn"
-          variant="outlined"
-          color="secondary"
-          helperText="Escribe el link a tu LinkedIn"
-          onChange={(e) => setLinkedin(e.target.value)}
+            />
+          </Grid>
 
-        />
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="social_network_linkedin"
+              label="LinkedIn"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              helperText="Escribe el link a tu LinkedIn"
+              onChange={(e) => setLinkedin(e.target.value)}
+
+            />
+          </Grid>
+        </Grid>
+
 
         <TextField
           sx={{
@@ -390,45 +414,53 @@ export default function CreateProfileForm() {
 
         />
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="legal_structure"
-          select
-          required
-          label="Estructura legal"
-          helperText="Selecciona la estructura legal de tu empresa"
-          variant="outlined"
-          color="secondary"
-          onChange={(e) => setLegalStructure(e.target.value)}
-        >
-          {legalStructureOptions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="legal_structure"
+              select
+              required
+              label="Estructura legal"
+              helperText="Selecciona la estructura legal de tu empresa"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onChange={(e) => setLegalStructure(e.target.value)}
+            >
+              {legalStructureOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="sector"
-          select
-          required
-          label="Sector"
-          helperText="Selecciona el sector al que pertenece tu empresa"
-          variant="outlined"
-          color="secondary"
-          onChange={(e) => setSector(e.target.value)}
-        >
-          {sectorOptions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="sector"
+              select
+              required
+              label="Sector"
+              helperText="Selecciona el sector al que pertenece tu empresa"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onChange={(e) => setSector(e.target.value)}
+            >
+              {sectorOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+        </Grid>
 
         <TextField
           sx={{
@@ -458,84 +490,100 @@ export default function CreateProfileForm() {
 
         />
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="business_size"
-          select
-          required
-          label="Tamaño de la empresa"
-          helperText="Selecciona el tamaño de tu empresa"
-          variant="outlined"
-          color="secondary"
-          onChange={(e) => setBusinessSize(e.target.value)}
-        >
-          {BusinessSizeOptions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="business_size"
+              select
+              required
+              label="Tamaño de la empresa"
+              helperText="Selecciona el tamaño de tu empresa"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onChange={(e) => setBusinessSize(e.target.value)}
+            >
+              {BusinessSizeOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="market"
-          select
-          label="Mercado"
-          helperText="Selecciona el mercado de tu empresa"
-          variant="outlined"
-          color="secondary"
-          onChange={(e) => setMarket(e.target.value)}
-        >
-          {MarketOptions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="market"
+              select
+              label="Mercado"
+              helperText="Selecciona el mercado de tu empresa"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onChange={(e) => setMarket(e.target.value)}
+            >
+              {MarketOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+        </Grid>
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="sales_channel"
-          select
-          required
-          label="Canal de venta"
-          helperText="Selecciona el canal de venta de tu empresa"
-          variant="outlined"
-          color="secondary"
-          onChange={(e) => setSalesChannels(e.target.value)}
-        >
-          {ChannelOptions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="sales_channel"
+              select
+              required
+              label="Canal de venta"
+              helperText="Selecciona el canal de venta de tu empresa"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onChange={(e) => setSalesChannels(e.target.value)}
+            >
+              {ChannelOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
 
-        <TextField
-          sx={{
-            margin: '1rem',
-          }}
-          id="clients"
-          select
-          required
-          label="Clientes"
-          helperText="Selecciona el modelo de clientes de tu empresa"
-          variant="outlined"
-          color="secondary"
-          onChange={(e) => setClients(e.target.value)}
-        >
-          {ClientsOptions.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              sx={{
+                margin: '1rem',
+              }}
+              id="clients"
+              select
+              required
+              label="Clientes"
+              helperText="Selecciona el modelo de clientes de tu empresa"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              onChange={(e) => setClients(e.target.value)}
+            >
+              {ClientsOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+        </Grid>
 
 
 
