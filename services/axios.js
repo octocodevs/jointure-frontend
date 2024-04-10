c2a782a10d6b2212776b1dbff19ca3e0e9d6e38b
melitacasola <melissacasola@gmail.com>
2024-04-10 11:53:10 +0200

    clear axios file

--- services/axios.js
+++ services/axios.js
@@ -49,60 +49,6 @@ export const logoutUser = async (authToken) => {
     }
 }
 
-export const createCollaboration = async (collaborationData, authToken) => {
-    try {
-        console.log("collaborationData: inicio del axios", collaborationData);
-        console.log("authToken: comienzo del axios,  token", authToken);
-
-        const response = await axios.post("/api/marketplace", collaborationData, {
-            headers: {
-                "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN"),
-                Authorization: `Bearer ${authToken}`,
-            },
-        });
-
-        console.log(response, "respondiendo a creación de colaboración");
-
-        if (response && response.data) {
-            console.log("RESPONSE ?? qué llega?:", response.data);
-            return response.data;
-
-        } else {
-            console.error("La respuesta no contiene datos:", response);
-            throw new Error("La respuesta no contiene datos");
-        }
-    } catch (error) {
-        console.error("Error al crear collab:", error);
-        throw error;
-    }
-}
-
-export const updateCollaboration = async (collaborationId, collaborationData) => {
-    try {
-        const response = await axios.post(`api/marketplace/${collaborationId}`, collaborationData);
-        return response.data;
-    } catch (error) {
-        throw error.response.data;
-    }
-}
-
-//delete Collaboration
-export const deleteCollaboration = async (collaborationId, authToken) => {
-    try {
-        const response = await axios.delete(`api/marketplace/${collaborationId}`, collaborationId, {
-            headers: {
-                "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN"),
-                Authorization: `Bearer ${authToken}`,
-            },
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error al eliminar colaboración:", error);
-        throw error;
-    }
-};
-
-
 
 export const createNewProfile = async (userData) => {
     try {
